3339812718e631c94783355b7ce6d4037ed3c9ff
Emmanuil <emmanuil.litvinenko@inbox.ru>
2023-10-31 00:59:26 +0500

    fourth practice

---
+++ 4JS/main.js
@@ -0,0 +1,64 @@
+function generatorArray(count, n, m) {
+  let array = [];
+  let start = 0;
+  for (start; start < count; start++) {
+    array.push(Math.round(Math.random() * Math.abs(n - m)) + Math.min(n, m));
+  }
+  console.log(array);
+}
+
+function shuffleArray(count) {
+  let array = [];
+  for (let i = 1; i <= count; i++) {
+    array.push(i);
+  }
+  let firstArr = array.slice();
+
+  for (let index in array) {
+    let rnd = Math.round(Math.random() * (array.length - 1));
+    [array[index], array[Math.round(rnd)]] = [
+      array[Math.round(rnd)],
+      array[index],
+    ];
+  }
+  console.log(firstArr, '--->', array);
+  return array;
+}
+
+function searchElement(array, element) {
+  for (let index in array) {
+    if (array[index] === element) {
+      console.log('Индекс элемента:', index);
+      return;
+    }
+  }
+  console.log('Элемент не найден');
+}
+
+function combiningArrays(firstArray, secondArray) {
+  for (let i in secondArray) {
+    firstArray.push(secondArray[i]);
+  }
+  console.log(firstArray);
+}
+
+// проверка задания 1
+generatorArray(100, 2, 100);
+generatorArray(50, 2, 5);
+generatorArray(70, 100, -5);
+generatorArray(42, -3, -10);
+
+// проверка задания 2
+let firstArray = shuffleArray(10);
+let secondArray = shuffleArray(5);
+let thirdArray = shuffleArray(7);
+let fourthArray = shuffleArray(3);
+
+// проверка задания 3
+searchElement(firstArray, 3);
+searchElement(secondArray, 1);
+searchElement(thirdArray, 7);
+searchElement(fourthArray, 10);
+
+// проверка задания 4
+combiningArrays([2, 2, 17, 21, 45, 12, 54, 31, 53], [12, 44, 23, 5]);
